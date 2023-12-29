63edc6674508cfc0057337f08cfb14efa25e75b8
yuniwink <yuniwink@naver.com>
2023-12-29 07:47:16 +0000

    chore: modify demo.py - generate ./dataset/.. folders

--- demo.py
+++ demo.py
@@ -13,16 +13,20 @@ class MainRunner:
         else:
             print(f"No main.py found in {self.folder}")
 
-def run_main_scripts_in_order(folders):
+def run_main_scripts_in_order(make_folder_list, folders):
     """
     Runs main.py scripts in the specified folders in order.
 
     Parameters:
         - folders (list): List of folder paths containing main.py scripts.
     """
+    for path in make_folder_list:
+        os.makedirs(path, exist_ok=True)
+    
     for folder in folders:
         runner = MainRunner(folder)
         runner.run_main_script()
-
+        
+make_folder_list = ['./dataset/image_extraction/video_01', './dataset/image_extraction/video_02', './dataset/result_txt']
 folders_to_run = ["YOLO", "frame_matching", "comparative_analysis"]
-run_main_scripts_in_order(folders_to_run)
+run_main_scripts_in_order(make_folder_list, folders_to_run)
